238f933c0cf1845326f908cc93c82017211a2c11
Karthika Raghavan <karthika.raghavan@gmail.com>
2022-02-27 12:53:06 -0800

    Added haproxy config files

---
+++ server_http_test.js
@@ -0,0 +1,13 @@
+const app = require("express") ();
+const port = process.env.PORT || 3000;
+
+app.get("/app1", (req,res)=> {
+    res.send(`Welcome to this express App running on ${port}`)
+});
+
+app.get("/app2", (req,res)=> {
+    res.send(`Welcome to this express App running on ${port}`)
+});
+
+
+app.listen(port, ()=>console.log(`On port: ${port}`));
